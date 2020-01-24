9aaaa5805ede6febdc1df9063e78b39f92a9a4fb
Sarah Allen <sarah@ultrasaurus.com>
2020-01-24 13:30:46 -0800

    this also works

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,44 @@ extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
 async fn pipe<'a, Reader: AsyncRead + Send + Unpin, Writer: AsyncWrite + Send + Unpin>
-    (inreader: Reader, inwriter:  Writer)
+(inreader: Reader, inwriter:  Writer)
 {
-    let mut reader = inreader;
-    let mut writer = inwriter;
-    let mut buf = [0; 1024];
-    // In a loop, read data from the src and write to the dest.
-    loop {
-        let n = match reader.read(&mut buf).await {
-            // socket closed
-            Ok(n) if n == 0 => {
-                info!("src socket closed");
-                return
-            },
-            Ok(n) => n,
-            Err(e) => {
-                eprintln!("failed to read from socket; err = {:?}", e);
-                return;
-            }
-        };
-        info!("read {} bytes", n);
+  let mut reader = inreader;
+  let mut writer = inwriter;
+  let mut buf = [0; 1024];
+  // In a loop, read data from the src and write to the dest.
+  loop {
+    let n = match reader.read(&mut buf).await {
+      // socket closed
+      Ok(n) if n == 0 => {
+        info!("src socket closed");
+        return
+      },
+      Ok(n) => n,
+      Err(e) => {
+        eprintln!("failed to read from socket; err = {:?}", e);
+        return;
+      }
+    };
+    info!("read {} bytes", n);
 
-        // Write the data back
-        if let Err(e) = writer.write_all(&buf[0..n]).await {
-            eprintln!("failed to write to socket; err = {:?}", e);
-            return;
-        }
+    // Write the data back
+    if let Err(e) = writer.write_all(&buf[0..n]).await {
+      eprintln!("failed to write to socket; err = {:?}", e);
+      return;
     }
+  }
 }
 
 #[tokio::main]
 async fn main() {
+  pretty_env_logger::init();
+  let args: Vec<String> = env::args().collect();
+  println!("{:?}", args);
+
+  let src_addr = "127.0.0.1:19355";
+  let dest_addr = "127.0.0.1:1935";
   tokio::spawn(async move {
-
-    pretty_env_logger::init();
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-
-    let src_addr = "127.0.0.1:19355";
-    let dest_addr = "127.0.0.1:1935";
 
     info!("listen on {}", src_addr);
     let mut listener = TcpListener::bind(src_addr).await.expect("tcp listen bind");
