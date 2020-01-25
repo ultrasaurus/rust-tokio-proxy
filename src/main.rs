7c93abf45da33b00296e5faeb5adea596901fe61
Sarah Allen <sarah@ultrasaurus.com>
2020-01-24 16:22:42 -0800

    cannot be sent between threads safely

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use futures::future::try_join;
 use std::env;
+use std::error::Error;
 use tokio::net::{TcpStream, TcpListener};
 use tokio::prelude::*;
-use tokio::io;
 
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
 async fn pipe<'a, Reader: AsyncRead + Send + Unpin, Writer: AsyncWrite + Send + Unpin>
-(inreader: Reader, inwriter:  Writer)
+(mut reader: Reader, mut writer:  Writer) -> std::result::Result<(), Box<dyn Error>>
 {
-  let mut reader = inreader;
-  let mut writer = inwriter;
   let mut buf = [0; 1024];
   // In a loop, read data from the src and write to the dest.
   loop {
@@ -18,12 +17,12 @@ async fn pipe<'a, Reader: AsyncRead + Send + Unpin, Writer: AsyncWrite + Send + 
       // socket closed
       Ok(n) if n == 0 => {
         info!("src socket closed");
-        return
+        return Ok(())  
       },
       Ok(n) => n,
       Err(e) => {
         eprintln!("failed to read from socket; err = {:?}", e);
-        return;
+        return Ok(())  
       }
     };
     info!("read {} bytes", n);
@@ -31,23 +30,23 @@ async fn pipe<'a, Reader: AsyncRead + Send + Unpin, Writer: AsyncWrite + Send + 
     // Write the data back
     if let Err(e) = writer.write_all(&buf[0..n]).await {
       eprintln!("failed to write to socket; err = {:?}", e);
-      return;
+      return Ok(())  
     }
   }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
   pretty_env_logger::init();
   let args: Vec<String> = env::args().collect();
   println!("{:?}", args);
 
   let src_addr = "127.0.0.1:19355";
   let dest_addr = "127.0.0.1:1935";
-  tokio::spawn(async move {
 
-    info!("listen on {}", src_addr);
-    let mut listener = TcpListener::bind(src_addr).await.expect("tcp listen bind");
+  info!("listen on {}", src_addr);
+  let mut listener = TcpListener::bind(src_addr).await.expect("tcp listen bind");
+  tokio::spawn(async move {
     let ( mut src, _) = listener.accept().await.expect("tcp listener accept");
     let ( src_reader, src_writer) = src.split();
 
@@ -56,12 +55,17 @@ async fn main() {
     dest.set_nodelay(true).expect("dest tcp set_nodelay");
     let ( dest_reader, dest_writer) = dest.split();
 
-    pipe(src_reader, dest_writer);
+    let dest_to_src = pipe(src_reader, dest_writer);
+    let src_to_dest = pipe(dest_reader, src_writer);
+
+    try_join(dest_to_src, src_to_dest).await.expect("try join");
+
   });
 
   let mut input = String::new();
   println!("press return to quit");
   std::io::stdin().read_line(&mut input).expect("stdio read_line");
+  Ok(())
 }
 
 
